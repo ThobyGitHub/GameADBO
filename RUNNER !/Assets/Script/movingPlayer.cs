7e145bc937eb009ea42d5647fa87dd6d100d8057
Thoby <gabriel.tobby@yahoo.co.id>
2016-11-01 13:28:47 +0700

    dodoUnity

---
+++ RUNNER !/Assets/Script/movingPlayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingPlayer : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public AudioSource jumpSound;
+    public bool grounded = true;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D myRigidBody;
+    private Collider2D myCollider;
+    private Animator myAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
+
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            if (grounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                jumpSound.Play();
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+            }
+        }
+        myAnimator.SetFloat("speed", myRigidBody.velocity.x);
+        myAnimator.SetBool("grounded", grounded);
+    }
+}
